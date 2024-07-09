2a575074c00080ec81a0fa98862ca3874dd6feac
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 13:41:19 +0200

    Add foojay plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,15 @@
 rootProject.name = "spoofax3-depman"
 
+// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
     }
+}
+
+// This downloads an appropriate JVM if not already available
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 include(":catalog")
